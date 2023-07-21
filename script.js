4e7ca63f96f599c399c7a55111fc14178616f001
Walker4real <frankkafweku84@gmail.com>
2023-07-21 05:44:29 -0700

    Fix the bug in the javaScript to optimize typing speed

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-var i = 0;
-var txt = "I am a front end developer with a strong passion of creating quality interactive websites that are easy to use and come with a great user experience";
-var speed = 20;
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("info").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+//Typing effect on first page
+var data = [
+    { text: "FRONT END DEVELOPER.", class: "title2" },
+    { text: "I am a front-end developer with a strong passion of creating quality interactive websites that are easy to use and come with a great user experience", class: "info" },
+    { text: "And here is yet another class.", class: "yet-another-class" }
+  ];
+  
+  function typeWriter(i) {
+    if (i < data.length) {
+      var currentData = data[i];
+      var j = 0;
+      var currentText = currentData.text;
+      var currentSpeed = 20;
+      
+      function typingEffect() {
+        if (j < currentText.length) {
+          document.querySelector("." + currentData.class).innerHTML += currentText.charAt(j);
+          j++;
+          setTimeout(typingEffect, currentSpeed);
+        }
+      }
+      
+      typingEffect();
+      i++;
+      setTimeout(function () {
+        typeWriter(i);
+      }, currentText.length * currentSpeed + 100); // Add a delay after each class is typed completely
+    }
   }
-}
-
-// Call typeWriter() when the page finishes loading
-window.onload = function () {
-  typeWriter();
-};
+  
+  // Call typeWriter() when the page finishes loading
+  window.onload = function () {
+    typeWriter(0);
+  };
+  
